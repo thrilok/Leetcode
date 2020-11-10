b9c296c41aca1b0b4bc16e893e09779654697d8d
thrilok <thrilok.reddy96@gmail.com>
2020-11-10 14:37:36 -0500

    Create Flipping an Image.py

---
+++ Flipping an Image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        for x in A:
+            x.reverse()
+            for y in range(0, len(x)):
+                if x[y] == 0:
+                    x[y] =1
+                else:
+                    x[y] =0
+        return A
+        
+# Runtime: 52 ms, faster than 53.41% of Python3 online submissions for Flipping an Image.
+# Memory Usage: 14.2 MB, less than 100.00% of Python3 online submissions for Flipping an Image.
